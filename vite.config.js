5f8c7d1672b51cb19dd34a5c344f424fa3381147
Tushal Pandey <your-email@example.com>
2026-06-18 17:48:08 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -14,11 +14,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    rollupOptions: {
-      external: [
-        'vite/modulepreload-polyfill'
-      ]
-    }
-  }
 })
